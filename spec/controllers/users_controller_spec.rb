75a94a0214e76c1818b519ce61a1071415ee784e
Devin Austin <devin.austin@gmail.com>
2013-10-25 17:19:32 -0600

    initial attempt at setting up rspec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -23,7 +23,8 @@ describe UsersController do
   # This should return the minimal set of attributes required to create a valid
   # User. As you add validations to User, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) { { "name" => "MyString" } }
+  # turn me into a helper
+  let(:valid_attributes) { { "name" => "MyString", "email" => "hank@stricklandpropane.com", "password" => "propane" } }
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
