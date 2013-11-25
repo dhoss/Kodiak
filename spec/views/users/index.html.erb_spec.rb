1b23e0b07b2c514168ec5ae01afc84dce66c2683
Devin Austin <devin.austin@gmail.com>
2013-11-24 21:51:23 -0700

    routing tests pass

--- spec/views/users/index.html.erb_spec.rb
+++ spec/views/users/index.html.erb_spec.rb
@@ -5,12 +5,10 @@ describe "users/index" do
     assign(:users, [
       stub_model(User,
         :name => "Name",
-        :password => "Password",
         :email => "Email"
       ),
       stub_model(User,
         :name => "Name",
-        :password => "Password",
         :email => "Email"
       )
     ])
@@ -20,7 +18,6 @@ describe "users/index" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "tr>td", :text => "Name".to_s, :count => 2
-    assert_select "tr>td", :text => "Password".to_s, :count => 2
     assert_select "tr>td", :text => "Email".to_s, :count => 2
   end
 end
--- spec/views/users/show.html.erb_spec.rb
+++ spec/views/users/show.html.erb_spec.rb
@@ -4,7 +4,6 @@ describe "users/show" do
   before(:each) do
     @user = assign(:user, stub_model(User,
       :name => "Name",
-      :password => "Password",
       :email => "Email"
     ))
   end
@@ -13,7 +12,6 @@ describe "users/show" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     rendered.should match(/Name/)
-    rendered.should match(/Password/)
     rendered.should match(/Email/)
   end
 end
