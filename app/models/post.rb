1a8865b83f77239373b7ca38932b8b9c3b1b2789
Devin Austin <devin.austin@gmail.com>
2013-12-16 14:09:48 -0700

    initial post model tests pass

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,9 +2,11 @@ class Post < ActiveRecord::Base
   attr_accessible :body, :category, :parent, :title, :tags, :category, :user
   
   # has many
+  has_many :comments, class_name: "Post", foreign_key: "parent"
   has_and_belongs_to_many :tags
 
   # belongs to
+  belongs_to :post_comments, class_name: "Post"
   belongs_to :user
   belongs_to :category
 
@@ -25,5 +27,36 @@ class Post < ActiveRecord::Base
 
   def self.update_post
     
+  end
+
+  def self.add_comment(comment)
+    self.create(:comments =>comment)
+  end
+  # credit: http://justcramer.com/2010/05/30/scaling-threaded-comments-on-django-at-disqus/
+  def self.list_comments
+    self.find_by_sql ["WITH RECURSIVE cte (id, message, author, path, parent_id, depth)  AS (
+                         SELECT  id,
+                                 body,
+                                 user_id,
+                                 array[id] AS path,
+                                 parent_id, 1 AS depth
+                         FROM    posts
+                         WHERE   parent_id IS NULL
+                         AND     id=?
+
+                         UNION ALL
+
+                         SELECT  comments.id,
+                                 comments.body,
+                                 comments.user_id,
+                                 cte.path || comments.id,
+                                 comments.parent_id,
+                                 cte.depth + 1 AS depth
+                         FROM    posts
+                         JOIN cte ON posts.parent_id = cte.id
+                       )
+                       SELECT id, message, author, path, depth 
+                       FROM cte 
+                       ORDER BY path;", self.id]
   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,30 @@
 require 'spec_helper'
+require'factory_girl'
 
 describe Post do
-  pending "add some examples to (or delete) #{__FILE__}"
+  include_context 'test_preparation'
+  include_context 'posts'
+  include_context 'users'
+
+  before(:each) do
+    setup
+  end
+
+  after(:each) do
+    teardown
+  end
+
+  let!(:user) { FactoryGirl.create(:user) }
+  subject(:post) { FactoryGirl.create(:post, user: user, :tags_attributes => [{ :name => "farty" }]) }
+
+  context "with no comments" do
+    it "has all required fields" do
+      expect(post.comments).to     be_empty
+      expect(post.category).not_to be_nil
+      expect(post.tags).not_to     be_empty
+      expect(post.user).to         eq(user)
+    end
+  end
+
+
 end
