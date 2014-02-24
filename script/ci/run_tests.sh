6cfa25a6d94b0f6419102bcfe81a5bdb51790ce2
Devin Austin <devin.austin@gmail.com>
2014-02-23 18:27:52 -0700

    trying again

--- script/ci/run_tests.sh
+++ script/ci/run_tests.sh
@@ -1,4 +1,5 @@
-psql -U postgres kodiak_test < ./sql/install_trigram_and_fuzzy_search_extensions.sql;
+echo `ls -la`
+psql -U postgres kodiak_test < sql/install_trigram_and_fuzzy_search_extensions.sql;
 export LANG=en_US.UTF-8; 
 export LC_ALL=en_US.UTF-8;
 RAILS_ENV=test bundle exec rake db:migrate
