9c61792006606e02ce83140b74568fffabeca02f
Devin Austin <devin.austin@gmail.com>
2014-05-20 14:17:56 -0600

    Revert "initial photos controller commit"
    This reverts commit b11e059db343d0d959d78eef5779f00dd29f0166.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
 Kodiak::Application.routes.draw do
 
 
-  get "photos/new"
-  get "photos/create"
-  get "photos/update"
-  get "photos/edit"
-  get "photos/destroy"
-  get "photos/index"
-  get "photos/show"
   resources :search
   resources :attachments
 
