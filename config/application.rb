96a574df7adb7c7e140bd11e42b24536c84ef981
Devin Austin <devin.austin@gmail.com>
2014-05-27 14:22:06 -0600

    added LESS compilation to config

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ end
 
 module Kodiak
   class Application < Rails::Application
+    config.less.paths << "#{Rails.root}/app/assets/stylesheets"
+    config.less.compress = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
