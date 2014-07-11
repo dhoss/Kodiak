2a568fdeabefee476d150ccfcaccd49c1304d999
Devin Austin <devin.austin@gmail.com>
2014-07-11 12:56:10 -0600

    initial user CRUD on model works

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,28 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:valid_user) {{ :email => "me@fart.com", :name => "Toots McGee", :password => "T00tpower!" }}
+  
+  describe "User" do
+    it "inserts a user" do
+      expect(User.create(valid_user)).to be_valid
+    end
+    it "finds a user" do 
+      User.create(valid_user)
+      expect(User.find_by(name: "Toots McGee").name).to eq("Toots McGee")
+    end
+    it "updates a user" do
+      user = User.create(valid_user)
+      user.name = "Pffft"
+      user.save
+      expect(User.first.name).to eq("Pffft")
+    end
+
+    it "deletes a user" do
+      user = User.create(valid_user)
+      expect(User.count).to eq(1)
+      user.delete
+      expect(User.count).to eq(0)
+    end
+  end
 end
