460b7beddde01fdf59371f04c1fbb9eacafff3f3
Devin Austin <devin.austin@gmail.com>
2014-07-29 11:05:22 -0600

    initial tag tests pass

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
+require 'factory_girl'
 
 describe Tag do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:user) { FactoryGirl.create(:user) }
+  let!(:post) { FactoryGirl.create(:post, user: user) }
+  describe "Basic CRUD" do
+    it "can add tags to an existing post." do
+      post.tags << Tag.create(name: "pffft")
+      expect(post.tags.count).to eq(1)
+    end
+  end
+
 end
