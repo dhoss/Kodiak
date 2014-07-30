e198c360b1646f50540920ddb3a553e1d9bbc512
Devin Austin <devin.austin@gmail.com>
2014-07-30 15:41:43 -0600

    initial failing archives routing test

--- spec/routing/archives_routing_spec.rb
+++ spec/routing/archives_routing_spec.rb
@@ -2,8 +2,14 @@ require 'spec_helper'
 
 describe ArchivesController do
   describe "routing" do
-    it "routes to #year"
-    it "routes to #month"
-    it "routes to #day"
+    it "routes to #year" do 
+      get("/2014").should route_to("archives#year")
+    end
+    it "routes to #month" do
+      get("/2014/07").should route_to("archives#month")
+    end
+    it "routes to #day" do
+      get("/2014/07/30").should route_to("archives#day")
+    end
   end
 end
