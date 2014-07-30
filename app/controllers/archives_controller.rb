1792d642a699f810e67f3ff450e3b421e85156d7
Devin Austin <devin.austin@gmail.com>
2014-07-30 16:36:27 -0600

    initial archives#index tests pass

--- app/controllers/archives_controller.rb
+++ app/controllers/archives_controller.rb
@@ -1,5 +1,11 @@
 class ArchivesController < ApplicationController
   def index 
+    @posts = Post.page(params[:page]).order(created_at: :desc)
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @posts }
+    end
   end
 
   def year
--- spec/controllers/archives_controller_spec.rb
+++ spec/controllers/archives_controller_spec.rb
@@ -1,5 +1,29 @@
 require 'spec_helper'
 
 describe ArchivesController do
+  include_context 'posts'
+  include_context 'users'
+
+  let!(:user) { User.create! user_attributes }
+  let!(:user_post) { user.posts.create! post_attributes }
+
+  describe "GET index" do
+    it "lists all posts sorted by year" do
+      get :index
+      assigns(:posts).should eq([user_post])
+    end
+  end
+
+  describe "GET year" do
+    it "lists posts by a specified year"
+  end
+
+  describe "GET month" do
+    it "lists posts by a specified month"
+  end
+
+  describe "GET day" do
+    it "lists post by a specified day"
+  end
 
 end
