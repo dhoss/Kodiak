6a990791c8ac2b27e409289e7e0b9699a6641369
Devin Austin <devin.austin@gmail.com>
2014-07-31 13:38:16 -0600

    initial archives#index tests pass

--- spec/controllers/archives_controller_spec.rb
+++ spec/controllers/archives_controller_spec.rb
@@ -5,17 +5,27 @@ describe ArchivesController do
   include_context 'users'
 
   let!(:user) { User.create! user_attributes }
-  let!(:user_post) { user.posts.create! post_attributes }
+  let!(:post_2014) { user.posts.create! post_attributes }
+  let!(:post_2013) { user.posts.create! post_attributes }
+  before(:each) do
+    post_2014.created_at = DateTime.new(2014, 7, 31)
+    post_2014.save
+
+    post_2013.created_at = DateTime.new(2013, 8, 1)
+    post_2013.save
+  end
 
   describe "GET index" do
     it "lists all posts sorted by year" do
       get :index
-      assigns(:posts).should eq([user_post])
+      assigns(:posts).map {|d| d.created_at.year }.should eq %(2014 2013)
     end
   end
 
   describe "GET year" do
-    it "lists posts by a specified year"
+    it "lists posts by a specified year" 
+
+
   end
 
   describe "GET month" do
