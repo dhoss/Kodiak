9b7b1f82a6bb17634a7ff83c7f2ee509e2e9c69d
Devin Austin <devin.austin@gmail.com>
2014-08-12 14:22:00 -0600

    added initialization in spec_helper for code climate and added token env variable in run_tests.sh

--- script/ci/run_tests.sh
+++ script/ci/run_tests.sh
@@ -1,4 +1,5 @@
 export LANG=en_US.UTF-8; 
 export LC_ALL=en_US.UTF-8;
+export CODECLIMATE_REPO_TOKEN=3d255b2cfd37d4db29b81c8ec30c17bac274d5b062024b8254cba978134309ee 
 RAILS_ENV=test bundle exec rake db:migrate
 RAILS_ENV=test bundle exec rspec
