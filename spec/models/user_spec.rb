bc35c0da868dcce0bfd4f7068769c932523d4b22
Devin Austin <devin.austin@gmail.com>
2014-08-14 17:47:06 -0600

    initial role tests pass

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,63 +1,67 @@
 require 'spec_helper'
 
 describe User do
-  let(:valid_user) {{ :email => "me@fart.com", :name => "Toots McGee", :password => "T00tpower!" }}
-  
+  before :each do
+    @valid_user = { :email => "me@fart.com", :name => "Toots McGee", :password => "T00tpower!", :roles => [Role.find_or_create_by(name: "poster")] }
+    @user =  User.create(@valid_user) 
+  end
+
   describe "Basic CRUD" do
     it "inserts a user" do
-      expect(User.create(valid_user)).to be_valid
+      expect(@user).to be_valid
     end
     it "finds a user" do 
-      User.create(valid_user)
       expect(User.find_by(name: "Toots McGee").name).to eq("Toots McGee")
     end
     it "updates a user" do
-      user = User.create(valid_user)
-      user.name = "Pffft"
-      user.save
+      @user.name = "Pffft"
+      @user.save
       expect(User.first.name).to eq("Pffft")
     end
 
     it "deletes a user" do
-      user = User.create(valid_user)
       expect(User.count).to eq(1)
-      user.delete
+      @user.delete
       expect(User.count).to eq(0)
     end
   end
 
   describe "Relationships" do
 
-    let(:user) { User.create(valid_user) }
-    
     it "starts with no posts" do
-      expect(user.posts.count).to eq(0)
+      expect(@user.posts.count).to eq(0)
     end
     
     it "creates a post" do
-      user.posts.create(title: "Fart", body: "Pffft")
-      expect(user.posts.count).to eq(1)
+      @user.posts.create(title: "Fart", body: "Pffft")
+      expect(@user.posts.count).to eq(1)
     end
     
     it "updates a post" do
-      post = user.posts.create(title: "Fart", body: "Pffft")
+      post = @user.posts.create(title: "Fart", body: "Pffft")
       post.body = "fart nugget"
       post.save
       expect(post.body).to eq("fart nugget")
     end
     
     it "deletes a post" do
-      post = user.posts.create(title: "Frrrrp", body: "fart")
-      expect(user.posts.count).to eq(1)
-      user.posts.first.delete
-      expect(user.posts.count).to eq(0)
+      post = @user.posts.create(title: "Frrrrp", body: "fart")
+      expect(@user.posts.count).to eq(1)
+      @user.posts.first.delete
+      expect(@user.posts.count).to eq(0)
     end
     
     it "deletes cascade when owner is deleted" do
-      user.posts.create(title: "lj4law;l", body: "gkj3;ljgl3")
-      expect(user.posts.count).to eq(1)
-      user.delete
+      @user.posts.create(title: "lj4law;l", body: "gkj3;ljgl3")
+      expect(@user.posts.count).to eq(1)
+      @user.delete
       expect(Post.count).to eq(0)
+    end
+  end
+
+  describe "User Roles" do
+    it "starts with the 'poster' role" do
+      expect(@user.roles.count).to eq (1)
     end
   end
 end
