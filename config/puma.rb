721472e981fd430ce194d5627a10e6e067da51a9
Devin Austin <devin.austin@gmail.com>
2014-09-16 01:09:44 -0600

    attempting to fix the puma stuff

--- config/puma.rb
+++ config/puma.rb
@@ -1,9 +1,3 @@
-threads 1, 6
-workers 2
-
-on_worker_boot do
-  require "active_record"
-  cwd = ENV["PUMA_CONFIG"] || File.dirname(__FILE__)+"/.."
-  ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
-  ActiveRecord::Base.establish_connection(ENV["DATABASE_URL"] || YAML.load_file("#{cwd}/config/database.yml")[ENV["RAILS_ENV"]])
-end
+pidfile "/var/www/kodiak/current/tmp/puma/pid"
+state_path "/var/www/kodiak/current/tmp/puma/state"
+activate_control_app
