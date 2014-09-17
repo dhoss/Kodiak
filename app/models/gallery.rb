522323fa651141282c9450d5786d70e52cf2248d
Devin Austin <devin.austin@gmail.com>
2014-09-17 02:07:08 -0600

    updated default picture path and link 'back' after adding photos to a gallery

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -16,6 +16,6 @@ class Gallery < ActiveRecord::Base
   end
 
   def cover_photo
-    photos.first ? photos.first.attachment_url(:thumb) : ActionController::Base.helpers.asset_path("images/default.jpg")
+    photos.first ? photos.first.attachment_url(:thumb) : ActionController::Base.helpers.asset_path("default.jpg")
   end
 end
