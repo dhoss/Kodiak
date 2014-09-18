5debe122d92f637c705ca531ee5d8ea05ec74394
Devin Austin <devin.austin@gmail.com>
2014-09-18 16:02:15 -0600

    added fixes for categories, and some spec helpers for request tests. this closes #80.

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,10 +2,34 @@ require 'spec_helper'
 
 describe "Posts" do
   describe "GET /posts" do
-    it "works! (now write some real specs)" do
+    it "gets posts listing successfully" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       get posts_path
       response.status.should be(200)
     end
   end
+
+  describe "PUT /posts/:id" do
+    it "updates a post with a new category properly" do
+      sign_in
+      expect {
+        post posts_url, {
+          post: {
+            title: "pfffft",
+            body: "fllippppbbbbthhhhtttt",
+            new_category: "fart"
+          }
+        }
+      }.to change(Post, :count).by(1)
+
+      expect { 
+        put "/posts/#{Post.find_by(title: "pfffft").slug}", 
+          post: {
+            new_category: "pffft"
+          }
+        
+      }.to change(Category, :count).by(1)
+    end
+  end
+
 end
