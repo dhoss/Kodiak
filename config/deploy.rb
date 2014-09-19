6be0b3005db35d9caff292058d686fb160f4b30f
Devin Austin <devin.austin@gmail.com>
2014-09-19 01:49:01 -0600

    fixed url again

--- config/deploy.rb
+++ config/deploy.rb
@@ -62,7 +62,6 @@ task :deploy => :environment do
     invoke :'bundle:install'
     invoke :'rails:db_migrate'
     invoke :'rails:assets_precompile'
-    queue! %[ln -s /var/www/kodiak/shared shared]
 
     to :launch do
       queue "sudo service kodiak restart"
