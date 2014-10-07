70b549d25a0ca88487f89a5764f75193f9222888
Devin Austin <devin.austin@gmail.com>
2014-10-07 16:25:22 -0600

    updated category view

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,3 +1,4 @@
+require 'pp'
 class CategoriesController < ApplicationController
   before_action :set_category, only: [:show, :edit, :update, :destroy]
 
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,7 @@
 class Category < ActiveRecord::Base
   extend FriendlyId
   friendly_id :name, use: :slugged
+  has_many :posts
 
   validates :name, presence: true, uniqueness: true
 end
