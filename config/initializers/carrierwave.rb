e7e96a3713b735f79fcb9f22212083b5e846f4bb
Devin Austin <devin.austin@gmail.com>
2014-10-07 20:54:59 -0600

    changed aws keys

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
-    :aws_access_key_id      => 'AKIAJ7JTOFLAOTFSS4VA',                        # required
-    :aws_secret_access_key  => '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'                        # required
+    :aws_access_key_id      => 'AKIAIZZX4KTNY4WRRBSA',                        # required
+    :aws_secret_access_key  => '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'                        # required
   }  
   config.fog_directory  = Rails.env == "production" ? "stonecold-prod-images" : "stonecold-dev-images"                     # required
   config.fog_public     = false                                   # optional, defaults to true
