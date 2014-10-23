8e31ce2700d67f87bb829d5aaaa2e15416204a7a
Devin Austin <devin.austin@gmail.com>
2014-10-23 11:54:29 -0600

    updated domain

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ require 'mina/rvm'    # for rvm support. (http://rvm.io)
 #   repository   - Git repo to clone from. (needed by mina/git)
 #   branch       - Branch name to deploy. (needed by mina/git)
 
-set :domain, 'stonecolddev.in'
+set :domain, '104.131.76.145'
 set :deploy_to, '/var/www/kodiak'
 set :repository, 'git@bitbucket.org:djaustin/stonecolddev.in.git'
 set :branch, 'master'
