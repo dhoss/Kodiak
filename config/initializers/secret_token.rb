ec47a4e3abf1a7398d569f125878309938996aa4
Devin Austin <devin.austin@gmail.com>
2015-07-24 15:31:19 -0600

    added secrets.yml

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Kodiak::Application.config.secret_token = 'f32b0b88ef590410db40d7afa9c3c9bd2c42fe2a6db7ed9b187f96308670631df1249e59dcf3888b0ab7933d375cea0978fb08617f41c30232643d0831095999'
