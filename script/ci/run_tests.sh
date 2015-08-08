abc709b566e935f21f3dcaaf0af62c2480d3246d
Devin Austin <devin.austin@gmail.com>
2015-08-08 15:14:27 -0600

    forgot to update run_tests.sh

--- script/ci/run_tests.sh
+++ script/ci/run_tests.sh
@@ -1,5 +1,7 @@
 export LANG=en_US.UTF-8; 
 export LC_ALL=en_US.UTF-8;
 export CODECLIMATE_REPO_TOKEN=3d255b2cfd37d4db29b81c8ec30c17bac274d5b062024b8254cba978134309ee 
+RAILS_ENV=test bundle exec rake seed_migration:install:migrations
 RAILS_ENV=test bundle exec rake db:migrate
+RAILS_ENV=test bundle exec rake seed:migrate
 RAILS_ENV=test bundle exec rspec
